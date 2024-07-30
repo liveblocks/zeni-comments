7d69bbb889343324ca6b09a5ed3dc74e2f2c839e
Florent <florent.lefebvre@liveblocks.io>
2024-07-30 17:33:38 -0400

    add auth provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,41 +1,89 @@
 "use client";
 
 import { LiveblocksProvider } from "@liveblocks/react";
-import { PropsWithChildren } from "react";
+import React from "react";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
 
-export function Providers({ children }: PropsWithChildren) {
+export const AuthContext = createContext<{
+  token: null;
+  setToken: Dispatch<SetStateAction<null>>;
+} | null>(null);
+import { ReactNode } from "react";
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState(null);
   return (
-    <LiveblocksProvider
-      authEndpoint="/api/liveblocks-auth"
-      // Get users' info from their ID
-      resolveUsers={async ({ userIds }) => {
-        const searchParams = new URLSearchParams(
-          userIds.map((userId) => ["userIds", userId])
-        );
-        const response = await fetch(`/api/users?${searchParams}`);
-
-        if (!response.ok) {
-          throw new Error("Problem resolving users");
-        }
-
-        const users = await response.json();
-        return users;
-      }}
-      // Find a list of users that match the current search term
-      resolveMentionSuggestions={async ({ text }) => {
-        const response = await fetch(
-          `/api/users/search?text=${encodeURIComponent(text)}`
-        );
-
-        if (!response.ok) {
-          throw new Error("Problem resolving mention suggestions");
-        }
-
-        const userIds = await response.json();
-        return userIds;
-      }}
-    >
+    <AuthContext.Provider value={{ token, setToken }}>
       {children}
-    </LiveblocksProvider>
+    </AuthContext.Provider>
   );
-}
+};
+export const useAuth = () => useContext(AuthContext);
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState(null);
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      <LiveblocksProvider
+        authEndpoint={async (room) => {
+          const response = await fetch("/api/liveblocks-auth", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            // Don't forget to pass `room` down. Note that it
+            // can be undefined when using Notifications.
+            body: JSON.stringify({ room }),
+          });
+
+          const auth = await response.json();
+
+          const token = auth.token;
+
+          if (!token) {
+            throw new Error("Problem authenticating");
+          }
+
+          setToken(token); // Set the token in AuthProvider
+
+          return auth;
+        }}
+        // Get users' info from their ID
+        resolveUsers={async ({ userIds }) => {
+          const searchParams = new URLSearchParams(
+            userIds.map((userId) => ["userIds", userId])
+          );
+          const response = await fetch(`/api/users?${searchParams}`);
+
+          if (!response.ok) {
+            throw new Error("Problem resolving users");
+          }
+
+          const users = await response.json();
+          return users;
+        }}
+        // Find a list of users that match the current search term
+        resolveMentionSuggestions={async ({ text }) => {
+          const response = await fetch(
+            `/api/users/search?text=${encodeURIComponent(text)}`
+          );
+
+          if (!response.ok) {
+            throw new Error("Problem resolving mention suggestions");
+          }
+
+          const userIds = await response.json();
+          return userIds;
+        }}
+      >
+        {children}
+      </LiveblocksProvider>
+    </AuthContext.Provider>
+  );
+};
