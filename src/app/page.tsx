d83ed33b2ae7ac4a232c9161a1fdb831ff23f72c
Florent <florent.lefebvre@liveblocks.io>
2024-07-30 16:34:36 -0400

    refactor: Simplify customer row rendering logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,169 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { RoomProvider, useThreads } from "@liveblocks/react/suspense";
 import { Loading } from "../components/Loading";
-import { Composer, Thread } from "@liveblocks/react-ui";
-import { ClientSideSuspense } from "@liveblocks/react";
+import { ClientSideSuspense, useRoom } from "@liveblocks/react";
 import { ErrorBoundary } from "react-error-boundary";
+import { ThreadData } from "@liveblocks/core";
+import { Thread } from "@liveblocks/react-ui";
 
-/**
- * Displays a list of threads, along with a composer for creating
- * new threads.
- */
+const groups = ["Pro", "Growth", "Enterprise"];
 
-function Example() {
-  const { threads } = useThreads();
-
-  return (
-    <main>
-      {threads.map((thread) => (
-        <Thread key={thread.id} thread={thread} className="thread" />
-      ))}
-      <Composer className="composer" disabled={false} />
-    </main>
-  );
-}
+const plans = ["tax pro", "payroll", "accounting"];
 
 export default function Page() {
-  // 10 rooms are created for the example
   const [numberOfExamples, setNumberOfExamples] = useState(10);
+  const [focusedCustomerId, setFocusedCustomerId] = useState<string | null>(
+    null
+  );
 
-  const [exampleIds, setExampleIds] = useState(() =>
+  const [customerIds, setcustomerIds] = useState(() =>
     Array.from({ length: numberOfExamples }, (_, i) => i)
   );
 
   useEffect(() => {
-    setExampleIds((exampleIds) => [
-      ...exampleIds,
+    setcustomerIds((customerIds) => [
+      ...customerIds,
       ...Array.from(
-        { length: numberOfExamples - exampleIds.length },
-        (_, i) => exampleIds.length + i
+        { length: numberOfExamples - customerIds.length },
+        (_, i) => customerIds.length + i
       ),
     ]);
   }, [numberOfExamples]);
 
   return (
-    <div>
-      {exampleIds.map((exampleId) => (
-        <Room key={exampleId} exampleId={exampleId.toString()} />
-      ))}
-      {/* Load more button */}
-      <button
-        className="load-more"
-        onClick={() => setNumberOfExamples(numberOfExamples + 10)}
-      >
-        Load more
-      </button>
-    </div>
+    <table style={{ width: "100%", margin: 50 }}>
+      <thead>
+        <tr>
+          <th style={{ width: "20%" }}>Name</th>
+          <th style={{ width: "20%" }}>Group</th>
+          <th style={{ width: "20%" }}>Plan</th>
+          <th style={{ width: "20%" }}>Using RoomProvider</th>
+          <th style={{ width: "20%" }}>Thread</th>
+        </tr>
+      </thead>
+      <tbody>
+        {customerIds.map((customerId) => (
+          <CustomerRow
+            key={customerId}
+            customerId={customerId.toString()}
+            onClick={setFocusedCustomerId}
+            focused={focusedCustomerId === customerId.toString()}
+          />
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={4}>
+            <button
+              className="load-more"
+              onClick={() => setNumberOfExamples(numberOfExamples + 10)}
+            >
+              Load more
+            </button>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
   );
 }
 
-function Room(props: { roomId?: string; exampleId?: string }) {
-  // const roomId = useExampleRoomId("liveblocks:examples:nextjs-comments");
-  const roomId =
-    props.roomId ?? props.exampleId
-      ? `liveblocks:examples:nextjs-comments:${props.exampleId}`
-      : "liveblocks:examples:nextjs-comments";
+function CustomerRow({
+  customerId,
+  focused,
+  onClick,
+}: {
+  customerId: string;
+  focused: boolean;
+  onClick: (customerId: string) => void;
+}) {
+  if (focused) {
+    return (
+      <RoomProvider id={`liveblocks:examples:nextjs-comments:${customerId}`}>
+        <LiveblocksRow
+          customerId={customerId}
+          onClick={onClick}
+          focused={focused}
+        />
+      </RoomProvider>
+    );
+  }
+
   return (
-    <RoomProvider
-      id={roomId}
-      autoConnect={false}
-      // enablePolling={true}
+    <Row
+      customerId={customerId}
+      onClick={onClick}
+      focused={focused}
+      thread={null}
+    />
+  );
+}
+
+function LiveblocksRow({
+  customerId,
+  onClick,
+  focused,
+}: {
+  customerId: string;
+  onClick: (customerId: string) => void;
+  focused: boolean;
+}) {
+  const room = useRoom();
+
+  const { threads } = useThreads();
+
+  useEffect(() => {
+    return () => {
+      console.log("unmounting", customerId);
+      room.disconnect();
+    };
+  });
+
+  return (
+    <ErrorBoundary
+      fallback={
+        <div className="error">There was an error while getting threads.</div>
+      }
     >
-      <ErrorBoundary
-        fallback={
-          <div className="error">There was an error while getting threads.</div>
-        }
-      >
-        <ClientSideSuspense fallback={<Loading />}>
-          <Example />
-        </ClientSideSuspense>
-      </ErrorBoundary>
-    </RoomProvider>
+      <ClientSideSuspense fallback={<Loading />}>
+        <Row
+          customerId={customerId}
+          onClick={onClick}
+          focused={focused}
+          thread={threads.length > 0 ? threads[0] : null}
+        />
+      </ClientSideSuspense>
+    </ErrorBoundary>
+  ); 
+}
+
+function Row({
+  customerId,
+  onClick,
+  focused,
+  thread,
+}: {
+  customerId: string;
+  onClick: (customerId: string) => void;
+  focused: boolean;
+  thread: null | ThreadData;
+}) {
+  return (
+    <tr key={customerId} onClick={() => onClick(customerId)}>
+      <td>{"Customer " + customerId}</td>
+      <td>
+        {/* Random group using customerId value */}
+        {groups[parseInt(customerId) % groups.length]}
+      </td>
+      <td>
+        {/* Random plan or none "-" using customerId value */}
+        {parseInt(customerId) % 2 === 0
+          ? plans[parseInt(customerId) % plans.length]
+          : "-"}
+      </td>
+      <td>{focused.toString()}</td>
+      <td>{thread ? <Thread thread={thread} /> : "-"}</td>
+    </tr>
   );
 }
