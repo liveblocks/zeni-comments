f11c1195d77f46ead8f763133787e61ae329a857
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-07-31 17:59:04 +0200

    Fix endpoint

--- src/app/api/liveblocks-user-threads/route.ts
+++ src/app/api/liveblocks-user-threads/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { LIVEBLOCKS_BASE_URL } from "../../../constants";
 
+export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
+  console.log(request.bodyUsed);
   const res = await fetch(`${LIVEBLOCKS_BASE_URL}/v2/users/user-1/threads`, {
     headers: {
       Authorization: `Bearer ${process.env.LIVEBLOCKS_SECRET_KEY}`,
